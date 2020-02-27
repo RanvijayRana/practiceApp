8df82601296aa839b0c05fb4f8ada0d499a31022
mr ranvijay <ranvijay.cse@outlook.com>
2020-02-27 11:40:18 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ChildComponent } from './child/child.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChildComponent,
+    HeaderComponent,
+    RecipesComponent
   ],
   imports: [
     BrowserModule
